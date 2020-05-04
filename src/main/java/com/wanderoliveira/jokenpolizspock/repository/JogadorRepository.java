475f27f4575afa1d59df22a38f78d3a81587ad2f
Wander <wander.luis1@gmail.com>
2020-05-04 02:13:40 -0300

    Revert "projeto jokenpolizspock-api"
    This reverts commit 96fdbb96cf4954011cbe7e7503af8e466cf33c3c.

--- src/main/java/com/wanderoliveira/jokenpolizspock/repository/JogadorRepository.java
+++
@@ -1,9 +0,0 @@
-package com.wanderoliveira.jokenpolizspock.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.wanderoliveira.jokenpolizspock.datasource.model.Jogador;
-
-public interface JogadorRepository extends JpaRepository<Jogador, Long> {
-
-}
--- src/main/resources/sql/insert_Jogadores_para_test_buscar_por_id.sql
+++
@@ -1,4 +0,0 @@
-delete from public.jogador
-INSERT INTO public.jogador
-(id, jogada, nome)
-VALUES(1, 'PEDRA', 'JOGADOR 1');
